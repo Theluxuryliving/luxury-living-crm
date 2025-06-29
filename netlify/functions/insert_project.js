5c461db1c10efa905ffb1671f40817015710d217
Atif Zubair <atif.zubair@gmail.com>
2025-06-29 23:43:53 +0500

    Fix syntax in insert_project.js

--- netlify/functions/insert_project.js
+++ netlify/functions/insert_project.js
@@ -4,16 +4,16 @@ exports.handler = async (event) => {
   try {
     const data = JSON.parse(event.body);
 
-     const connection = await mysql.createConnection({
+    const connection = await mysql.createConnection({
       host: 'sql311.infinityfree.com',
       user: 'if0_39342524',
       password: 'Orient009',
-      database: 'if0_39342524_theluxliving','
+      database: 'if0_39342524_theluxliving'
     });
 
     await connection.execute(
-      'INSERT INTO crm_projects (project_name, developer, ...) VALUES (?, ?, ...)',
-      [data.project_name, data.developer /* etc */]
+      `INSERT INTO crm_projects (project_name, developer) VALUES (?, ?)`,
+      [data.project_name, data.developer] // extend this array to match your actual fields
     );
 
     await connection.end();
@@ -22,6 +22,7 @@ exports.handler = async (event) => {
       statusCode: 200,
       body: JSON.stringify({ success: true, message: 'Project saved!' })
     };
+
   } catch (err) {
     return {
       statusCode: 500,
@@ -29,5 +30,3 @@ exports.handler = async (event) => {
     };
   }
 };
- 
-   
